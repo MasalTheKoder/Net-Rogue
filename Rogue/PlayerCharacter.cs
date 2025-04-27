b6df6586e989e94d24eec64086bda4aba6d5359e
MasalTheKoder <s2201207@edu.bc.fi>
2025-04-27 19:25:05 +0300

    Hirveetä
    Hirveetä vaihtaa net 8.0 -> 6.0
    Teen tän commitin vaan koska en haluu enää leikkiä tän kaa

--- Rogue/PlayerCharacter.cs
+++ Rogue/PlayerCharacter.cs
@@ -4,17 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
+using ZeroElectric.Vinculum;
 
 namespace Rogue
 {
-
-    
-   public enum Race
+    public enum Race
     {
         Human,
         Elf,
         Orc
-
     }
 
     public enum Class
@@ -29,14 +27,21 @@ namespace Rogue
         public string name;
         public Race rotu;
         public Class luokka;
-
         public Vector2 paikka;
+        public Color playerColor;
+
+        public PlayerCharacter(string name, Race rotu, Class luokka, Color color)
+        {
+            this.name = name;
+            this.rotu = rotu;
+            this.luokka = luokka;
+            this.playerColor = color;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition((int)paikka.X, (int)paikka.Y);
             Console.Write("@");
         }
     }
-
-   
 }
